cd5b1bd9672363c52251a20454afbfc0c5cb1fff
mason369 <maosenyang00@gmail.com>
2022-09-18 19:55:47 +0800

    update

--- LyrePlayer/app/src/main/java/top/weixiansen574/LyrePlayer/MainActivity.java
+++ LyrePlayer/app/src/main/java/top/weixiansen574/LyrePlayer/MainActivity.java
@@ -37,7 +37,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button openFloatList;
     Button selectFromServer;
     SharedPreferences server;
+    Button crashButton = new Button(this);
+crashButton.setText("Test Crash");
+crashButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+            throw new RuntimeException("Test Crash"); // Force a crash
+        }
+    });
 
+    addContentView(crashButton, new ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+                   ViewGroup.LayoutParams.WRAP_CONTENT));
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
